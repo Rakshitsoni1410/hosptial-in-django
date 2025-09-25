2dd7e89321835361087c146b6d0204bff69066ef
rakshitrsoni <rakshitrsoni@gmail.com>
2025-09-25 13:19:49 +0530

    Update urls.py
    xcxcb

--- hospital_project/hospital/urls.py
+++ hospital_project/hospital/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home,path='home.html'),
-    path('add_patient/', views.add_patient,path='patient.html'),
+    path('', views.home),
+    path('patient/', views.patient),
+    path('doctor/',views.doctor)
 ]
